070bc6c35997d8a8b70dae6fa32b71ffa3b8f583
u14e <467030463@qq.com>
2016-10-03 17:33:39 +0800

    add node-express/lesson-2 codes and modify README.md

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # node-start-demo
 A series of demo collect from others
+
+- [node-beginner](http://www.nodebeginner.org/index-zh-cn.html)
+- [node-express](http://www.ituring.com.cn/book/1485)
---
+++ node-express/lesson-2/app.js
@@ -0,0 +1,35 @@
+var http = require('http');
+var fs = require('fs');
+
+function serverStaticFile(response, pathname, contentType, responseCode) {
+    responseCode || (responseCode = 200);
+    fs.readFile(__dirname + pathname, function(error, data) {
+        if(error) {
+            response.writeHead(500, {'Content-Type': 'text/plain'});
+            response.end('500 - Internal Error');
+        } else {
+            response.writeHead(responseCode, {'Content-Type': contentType});
+            response.end(data);
+        }
+    });
+}
+
+http.createServer(function(request, response) {
+    var pathname = request.url.replace(/\/?(?:\?.*)?$/, '').toLowerCase();
+    switch(pathname) {
+        case '':
+            serverStaticFile(response, '/public/home.html', 'text/html');
+            break;
+        case '/about':
+            serverStaticFile(response, '/public/about.html', 'text/html');
+            break;
+        case '/img/logo.png':
+            serverStaticFile(response, '/public/img/logo.png', 'image/png');
+            break;
+        default:
+            serverStaticFile(response, '/public/404.html', 'text/html', 404);
+            break;
+    }
+}).listen(8888);
+
+console.log('Server started on localhost:8888...');
